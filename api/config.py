2049ce1f9ae777b8bd70eb1c991ad0ad9563b42c
Brandon Dutton <brandonkdutton@gmail.com>
2021-01-08 00:32:08 -0800

    added config files

---
+++ api/config.py
@@ -0,0 +1,11 @@
+SECRET_KEY = '{secret key}'
+
+DATABASE = {
+    'host': '{host}',
+    'port': '{port}',
+    'user': '{user}',
+    'password': '{password}',
+    'db': '{db}',
+}
+
+# fill in this file with your own database connection info and secret key
